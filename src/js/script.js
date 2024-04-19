7a19bf3e4d07ab4ce6684aad68c6bb9698363bf4
sapar <saparmurzabekov@gmail.com>
2024-04-19 14:42:03 +0600

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const humburger = document.querySelector('.humburger'),
+    menu = document.querySelector('.menu'),
+    close = document.querySelector('.menu__close');
+
+    
+humburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+
+close.addEventListener('click',() =>{
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
+
